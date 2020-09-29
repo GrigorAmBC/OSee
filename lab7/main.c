1abd7288fbffd66b3262d958ec031a73aed6ff10
Grigor <32815456+GrigorAmBC@users.noreply.github.com>
2020-09-29 09:04:22 +0700

    Update main.c

--- lab7/main.c
+++ lab7/main.c
@@ -1,70 +1,97 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 
 #define THREAD_CREATION_SUCCESS 0
 #define THREAD_JOIN_SUCCESS 0
-#define BUFFER_SIZE 256
 #define EXIT_ERROR 1
 #define DECIMAL_CONSTANT 0
 #define num_steps 200000000
+#define THREAD_NUMBER_TOP_LIMIT 300
+#define NO_ERROR 0
 
 struct param{
     long long threadNumber;
     double result;
+    int threadsAmount;
 } typedef params;
 
-void printError(int code, char **argv) {
-    char buf[BUFFER_SIZE];
-    strerror_r(code, buf, sizeof buf);
-    printf("%s: %s\n", argv[0], buf);
+void printError(int code, char* error) {
+    char* buf = strerror(code);
+    printf("%s: %s\n", error, buf);
 }
 
-long int threadsAmount;
-
-void* calculate(void* param) {
+void* calculate(void* arg) {
     double localpi = 0.0;
-    long long i = ((params *)param)->threadNumber;
-    for (i; i < num_steps ; i += threadsAmount) {
+    long long i;
+    params* param = (params *) arg;
+
+    for (i = param->threadNumber; i < num_steps ; i += param->threadsAmount) {
         localpi += 1.0 / (i * 4.0 + 1.0);
         localpi -= 1.0 / (i * 4.0 + 3.0);
     }
-    printf("Thread %lld finished, partial sum %.16f\n", ((params *)param)->threadNumber, localpi);
-    ((params *)param)->result = localpi;
+
+    param->result = localpi;
     return param;
-} 
+}
+
+int parseArguments(int argc, char **argv) {
+    char* ret;
+    int numOfThreads;
+
+    if (argc != 2) {
+        printf("Usage %s [number of threads] format\n", argv[0]);
+        exit(EXIT_ERROR);
+    }
+
+    numOfThreads = strtol(argv[1], &ret, DECIMAL_CONSTANT);
+    if (errno != NO_ERROR) {
+        printError(errno, "Failed to convert argument to number");
+        exit(EXIT_ERROR);
+    }
+
+    if (ret[0] != '\0') {
+        printf("Only number available as argument\n");
+        exit(EXIT_ERROR);
+    }
+
+    if (numOfThreads < 1) {
+        printf("Invalid number of threads: %d\n", numOfThreads);
+        exit(EXIT_ERROR);
+    }
+
+    return numOfThreads;
+}
 
 int main(int argc, char** argv) {
     double pi = 0;
     int i, code;
+    long int threadsAmount;
     pthread_t* threads;
     params* param;
 
-    if (argc > 1) {
-        char* non_convertible;
-        threadsAmount = strtol(argv[1], &non_convertible, DECIMAL_CONSTANT);
-        if (*non_convertible != NULL) {
-            fprintf(stderr, "thread number argument must be a number\n");
-            exit(EXIT_ERROR);
-        }
-    } else {
-        fprintf(stderr, "requires an argument\n");
+    threadsAmount = parseArguments(argc, argv);
+
+    threads = (pthread_t*) malloc(threadsAmount * sizeof(pthread_t));
+	if (threads == NULL) {
+        printError(errno, "Failed to alloc memory for threads");
         exit(EXIT_ERROR);
     }
 
-    if (threadsAmount < 1) { 
-        fprintf(stderr, "usage: %s threadnum\n", argv[0]);
+    param = (params*) malloc(threadsAmount * sizeof(params));
+    if (param == NULL) {
+        printError(errno, "Failed to alloc memory for args");
         exit(EXIT_ERROR);
     }
 
-    param = malloc(threadsAmount * sizeof(params));
-    threads = malloc(threadsAmount * sizeof(pthread_t));				
     for(i = 0; i < threadsAmount; i++) {
         param[i].threadNumber = i;
-        code = pthread_create(threads + i, NULL, calculate, (void*)(param + i));
+		param[i].threadsAmount = threadsAmount;
+        code = pthread_create(threads + i, NULL, calculate, &param[i]);
         if (code != THREAD_CREATION_SUCCESS){
-            printError(code, argv);
+            printError(code, "Failed to create thread");
             exit(EXIT_ERROR);
         }
     }
@@ -72,16 +99,16 @@ int main(int argc, char** argv) {
     for(i = 0; i < threadsAmount; i++) {
         code = pthread_join(threads[i], NULL);
         if (code != THREAD_JOIN_SUCCESS) {
-            printError(code, argv);
+            printError(code, argv[0]);
             exit(EXIT_ERROR);
         }
         pi += param[i].result;
     }
     pi *= 4.0;
-    printf ("pi = %.16f\n", pi);
-	
+    printf("pi = %.16f\n", pi);
+
     free(param);
     free(threads);
-	
+
     pthread_exit(NULL);
 }
